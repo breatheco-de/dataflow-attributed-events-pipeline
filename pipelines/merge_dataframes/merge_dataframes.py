b68b9cac555cdcfc04c40becbbd5d79c6eb3cf45
Charlytoc <charlyjchaconc@gmail.com>
2023-08-07 20:33:50 +0000

    almost ready

--- pipelines/merge_dataframes/merge_dataframes.py
+++ pipelines/merge_dataframes/merge_dataframes.py
@@ -24,102 +24,78 @@ expected_output = pd.DataFrame({
     'title': ['aa','bb','bb']
 })
 
-def count_projects(json_data):
-    """
-    This function counts the number of projects in the provided JSON data.
-    """
-    count = 0
-    if 'days' in json_data:
-        for day in json_data['days']:
-            if 'project' in day:
-                count += 1
-    return count
-
-def calculate_attendance_for_user(data, user_id):
-    # Initialize counts for attendance and unattendance
-    total_attendance = 0
-    total_unattendance = 0
-
-    # Check if data is a dictionary or a list
-    if isinstance(data, dict):
-        data_to_iterate = data.values()
-    elif isinstance(data, list):
-        data_to_iterate = data
-    else:
-        raise ValueError('data should be either a dictionary or a list')
-
-    # Iterate over days
-    for day in data_to_iterate:
-        # If 'attendance_ids' or 'unattendance_ids' is None or not a list, skip this day
-        if not isinstance(day.get('attendance_ids'), list) or not isinstance(day.get('unattendance_ids'), list):
-            continue
-
-        # If user_id is in attendance_ids for the day, increment total_attendance
-        if user_id in day['attendance_ids']:
-            total_attendance += 1
-        # If user_id is in unattendance_ids for the day, increment total_unattendance
-        if user_id in day['unattendance_ids']:
-            total_unattendance += 1
-
-    # Calculate total days
-    total_days = total_attendance + total_unattendance
-
-    # Calculate attendance percentage
-    if total_days > 0:
-        attendance_percentage = (total_attendance / total_days) * 100
-    else:
-        attendance_percentage = 0.0
-
-    return attendance_percentage
 
 
-def get_attendance_percentage_for_user(df2, user_id):
-    user_attendance_percentages = []
-    for _, row in df2.iterrows():
-        history_log = row['history_log']
-        percentage = calculate_attendance_for_user(history_log, user_id)
-        user_attendance_percentages.append(percentage)
+def add_dummy_rows(df, df2):
+    # Define dummy data for df
+    dummy_data_df = {
+        'event_date': ['2022-01-01', '2022-01-02', '2022-01-03'],
+        'event_name': ['dummy_event1', 'dummy_event2', 'dummy_event3'],
+        'user_id': [12345, '', ''],
+        'user_pseudo_id': ['dummy_pseudo1', 'dummy_pseudo1', 'dummy_pseudo1'],
+        'device.web_info.hostname': ['dummy_host1', 'dummy_host2', 'dummy_host3'],
+    }
+    # Create a dataframe from the dummy data
+    dummy_df = pd.DataFrame(dummy_data_df)
+    
+    # Concatenate the dummy dataframe with the original df
+    df = pd.concat([dummy_df, df], ignore_index=True)
 
-    # Take the mean of the attendance percentages
-    mean_percentage = sum(user_attendance_percentages) / len(user_attendance_percentages) if user_attendance_percentages else 0.0
-    return mean_percentage
+    # Define dummy data for df2
+    dummy_data_df2 = {
+        'email': ['dummy_email1', 'dummy_email2', 'dummy_email3'],
+        'id': ['dummy_id1', 'dummy_id2', 'dummy_id3'],
+        'lead_type': ['dummy_lead1', 'dummy_lead2', 'dummy_lead3'],
+        'deal_status': ['WON', 'WON', 'WON'],
+        'year_month': ['2022-01', '2022-02', '2022-03'],
+        'attribution_id': [12345, 12367, 12356]
+    }
+    # Create a dataframe from the dummy data
+    dummy_df2 = pd.DataFrame(dummy_data_df2)
+    
+    # Concatenate the dummy dataframe with the original df2
+    df2 = pd.concat([dummy_df2, df2], ignore_index=True)
+
+    return df, df2
 
 
 def run(df, df2):
-    """
-    This function takes care of merging both datasets, events and attendees.
-    """
-    # print('Shape of df before merge:', df.shape)
-    print('Columns in df:', df.columns.to_list())
-    # print('First 5 rows of df:\n', df.head(5))
-    # print()
+    # Step 1: Filter records where status is WON
+    df, df2 = add_dummy_rows(df, df2)
+    df2 = df2[df2['deal_status'] == 'WON']
 
-    print('Shape of df2 before merge:', df2.shape)
-    print('Columns in df2:', df2.columns.to_list())
+    # Selecting only the required columns
+    df2 = df2[['email', 'id', 'lead_type', 'deal_status', 'year_month', 'attribution_id']]
+    
+    # Selecting only the required columns from df
+    df = df[['event_date', 'event_name', 'user_id', 'user_pseudo_id', 'device.web_info.hostname']]
 
+    # Step 2: Create a dictionary that maps user_pseudo_id to user_id
+    pseudo_to_user_dict = {}
+    for attr_id in df2['attribution_id'].unique():
+        if pd.isna(attr_id):  # Skip if the attribution_id is nan
+            continue
+        # Find user_id matching attribution_id
+        matching_user_ids = df[df['user_id'] == attr_id]['user_id']
+        if matching_user_ids.empty: # Check if there are no matching user_ids
+            continue
+        user_id = matching_user_ids.iloc[0]
+        # Find corresponding user_pseudo_id(s)
+        pseudo_ids = df[df['user_id'] == attr_id]['user_pseudo_id'].unique()
+        # Map user_pseudo_id to user_id
+        for pseudo_id in pseudo_ids:
+            pseudo_to_user_dict[pseudo_id] = user_id
 
-    # Convert NaN to empty strings or any default value
-    df2['json'].fillna('{}', inplace=True)
+    # Step 3: Fill user_id for all records with the same user_pseudo_id
+    for pseudo_id, user_id in pseudo_to_user_dict.items():
+        df.loc[df['user_pseudo_id'] == pseudo_id, 'user_id'] = user_id
 
-    # Now, ensure all data is of str type
-    df2['json'] = df2['json'].astype(str)
+    # Filter df and df2 to exclude rows where user_id or attribution_id is NaN
+    if 'attribution_id' in df.columns:
+        df = df[~df['user_id'].isna() & ~df['attribution_id'].isna()]
+    else:
+        df = df[~df['user_id'].isna()]
 
-    # Now you can parse the 'json' column into json objects
-    df2['json'] = df2['json'].apply(json.loads)
-
-
-
-   # Apply the count_projects function to each json object in the 'json' column of df2
-    df2['project_count'] = df2['json'].apply(count_projects)
-
-    # Merge the dataframes on 'cohort_id'
-    df = df.merge(df2[['cohort_id', 'project_count']], on='cohort_id', how='left')
-
-
-    df2['history_log'].fillna('{}', inplace=True)
-    df2['history_log'] = df2['history_log'].astype(str)
-    df2['history_log'] = df2['history_log'].apply(json.loads)
-
-    df['attendance_percentage'] = df['user_id'].apply(lambda user_id: get_attendance_percentage_for_user(df2, user_id))
+    df2 = df2[~df2['attribution_id'].isna()]
 
     return df
