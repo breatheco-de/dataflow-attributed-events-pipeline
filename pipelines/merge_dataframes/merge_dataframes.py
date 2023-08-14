d497148c79415f5d0c9c6579d522b25357a7e0b7
Charlytoc <charlyjchaconc@gmail.com>
2023-08-14 17:52:42 +0000

    attributed pipeline updated and improved

--- pipelines/merge_dataframes/merge_dataframes.py
+++ pipelines/merge_dataframes/merge_dataframes.py
@@ -25,77 +25,53 @@ expected_output = pd.DataFrame({
 })
 
 
+def add_dummy_data(df_events, df_form_entries):
+    # Dummy data for df_events
+    dummy_df_events = pd.DataFrame({
+        'event_date': [20220101, 20220102, 20220103],
+        'event_name': ['Event1', 'Event2', 'Event3'],
+        'user_id': [None, None, 12],
+        'user_pseudo_id': [200, 200, 200],
+        'device.web_info.hostname': ['host1', 'host2', 'host3']
+    })
 
-def add_dummy_rows(df, df2):
-    # Define dummy data for df
-    dummy_data_df = {
-        'event_date': ['2022-01-01', '2022-01-02', '2022-01-03'],
-        'event_name': ['dummy_event1', 'dummy_event2', 'dummy_event3'],
-        'user_id': [12345, '', ''],
-        'user_pseudo_id': ['dummy_pseudo1', 'dummy_pseudo1', 'dummy_pseudo1'],
-        'device.web_info.hostname': ['dummy_host1', 'dummy_host2', 'dummy_host3'],
-    }
-    # Create a dataframe from the dummy data
-    dummy_df = pd.DataFrame(dummy_data_df)
+    # Add dummy data to original df_events
+    df_events = pd.concat([df_events, dummy_df_events], ignore_index=True)
+
+    # Dummy data for df_form_entries
+    dummy_df_form_entries = pd.DataFrame({
+        'id': [1, 2],
+        'email': ['email1@example.com', 'email2@example.com'],
+        'attribution_id': [12, 23],
+        'deal_status': ['WON', 'WON']
+    })
     
-    # Concatenate the dummy dataframe with the original df
-    df = pd.concat([dummy_df, df], ignore_index=True)
+    # Add dummy data to original df_form_entries
+    df_form_entries = pd.concat([df_form_entries, dummy_df_form_entries], ignore_index=True)
 
-    # Define dummy data for df2
-    dummy_data_df2 = {
-        'email': ['dummy_email1', 'dummy_email2', 'dummy_email3'],
-        'id': ['dummy_id1', 'dummy_id2', 'dummy_id3'],
-        'lead_type': ['dummy_lead1', 'dummy_lead2', 'dummy_lead3'],
-        'deal_status': ['WON', 'WON', 'WON'],
-        'year_month': ['2022-01', '2022-02', '2022-03'],
-        'attribution_id': [12345, 12367, 12356]
-    }
-    # Create a dataframe from the dummy data
-    dummy_df2 = pd.DataFrame(dummy_data_df2)
-    
-    # Concatenate the dummy dataframe with the original df2
-    df2 = pd.concat([dummy_df2, df2], ignore_index=True)
-
-    return df, df2
+    return df_events, df_form_entries
 
 
-def run(df, df2):
-    # Step 1: Filter records where status is WON
-    df, df2 = add_dummy_rows(df, df2)
-    df2 = df2[df2['deal_status'] == 'WON']
+def run(df_events, df_form_entries):
+    # Add dummy data
+    # df_events, df_form_entries = add_dummy_data(df_events, df_form_entries)
 
-    # Selecting only the required columns
-    df2 = df2[['email', 'id', 'lead_type', 'deal_status', 'year_month', 'attribution_id']]
-    
-    # Selecting only the required columns from df
-    df = df[['event_date', 'event_name', 'user_id', 'user_pseudo_id', 'device.web_info.hostname']]
+    # Filter dataframe with deal_status == 'WON'
+    df_form_entries_won = df_form_entries[df_form_entries['deal_status'] == 'WON']
 
-    # Step 2: Create a dictionary that maps user_pseudo_id to user_id
-    pseudo_to_user_dict = {}
-    for attr_id in df2['attribution_id'].unique():
-        if pd.isna(attr_id):  # Skip if the attribution_id is nan
-            continue
-        # Find user_id matching attribution_id
-        matching_user_ids = df[df['user_id'] == attr_id]['user_id']
-        if matching_user_ids.empty: # Check if there are no matching user_ids
-            continue
-        user_id = matching_user_ids.iloc[0]
-        # Find corresponding user_pseudo_id(s)
-        pseudo_ids = df[df['user_id'] == attr_id]['user_pseudo_id'].unique()
-        # Map user_pseudo_id to user_id
-        for pseudo_id in pseudo_ids:
-            pseudo_to_user_dict[pseudo_id] = user_id
+    # Selecting only the required columns from the events and forms
+    df_events_filtered = df_events[['user_id', 'user_pseudo_id']].dropna(subset=['user_id']).copy()
+    df_form_entries_won_filtered = df_form_entries_won[['attribution_id']].copy()
 
-    # Step 3: Fill user_id for all records with the same user_pseudo_id
-    for pseudo_id, user_id in pseudo_to_user_dict.items():
-        df.loc[df['user_pseudo_id'] == pseudo_id, 'user_id'] = user_id
+    # Merge dataframes on user_id and attribution_id to find matching user_ids
+    merged_df = df_events_filtered.merge(df_form_entries_won_filtered, left_on='user_id', right_on='attribution_id', how='inner')
 
-    # Filter df and df2 to exclude rows where user_id or attribution_id is NaN
-    if 'attribution_id' in df.columns:
-        df = df[~df['user_id'].isna() & ~df['attribution_id'].isna()]
-    else:
-        df = df[~df['user_id'].isna()]
+    # Iterate through the unique user_pseudo_ids to apply matching user_ids
+    for pseudo_id in merged_df['user_pseudo_id'].unique():
+        matching_user_id = merged_df.loc[merged_df['user_pseudo_id'] == pseudo_id, 'user_id'].iloc[0]
+        df_events.loc[df_events['user_pseudo_id'] == pseudo_id, 'user_id'] = matching_user_id
+    # Drop rows with empty user_id from the final DataFrame
+    final_df = df_events.dropna(subset=['user_id'])
 
-    df2 = df2[~df2['attribution_id'].isna()]
-
-    return df
+    final_df = final_df[['event_date', 'event_name', 'user_id', 'user_pseudo_id', 'device.web_info.hostname']]
+    return final_df
